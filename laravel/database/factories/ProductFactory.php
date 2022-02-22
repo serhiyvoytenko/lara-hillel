521e7104aaa5d14abf2fd06224ddb971d4ce966b
Serhii <serhiy.voytenko@dn.pebs.biz>
2022-02-22 16:55:33 +0200

    fix some bags

--- laravel/database/factories/ProductFactory.php
+++ laravel/database/factories/ProductFactory.php
@@ -22,11 +22,11 @@ class ProductFactory extends Factory
             'title' => $this->faker->word,
             'description' => $this->faker->realText,
             'short_description' => $this->faker->realText(50),
-            'sku' => $this->faker->unique()->numberBetween(1000,100000000),
-            'category_id' =>Category::inRandomOrder()->first(),//
-            'price' => $this->faker->randomFloat(2,1,100000),
-            'discount' => $this->faker->randomFloat(2,1,20),
-            'count' => $this->faker->randomFloat(0, 1,100000),
+            'sku' => $this->faker->unique()->numberBetween(1000, 100000000),
+            'category_id' => Category::inRandomOrder()->first(),//
+            'price' => $this->faker->randomFloat(2, 1, 100000),
+            'discount' => $this->faker->randomFloat(2, 1, 20),
+            'count' => $this->faker->randomFloat(0, 1, 100000),
             'image_id' => Image::inRandomOrder()->first(),
         ];
     }
