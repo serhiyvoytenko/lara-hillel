90f8182aa7ce533bc013b549c70ebf52cf1d0b95
Serhii <serhiy.voytenko@dn.pebs.biz>
2022-03-11 16:58:46 +0200

    add auth

--- laravel/app/Http/Controllers/HomeController.php
+++ laravel/app/Http/Controllers/HomeController.php
@@ -23,6 +23,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $x = 1 + "1%" + "$1";
+        return view('home', $x);
     }
 }
