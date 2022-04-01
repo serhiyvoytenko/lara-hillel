2f8a1d2f7a3f42051f2ddddfc2b92a8a20d1ce38
Serhii <serhiy.voytenko@dn.pebs.biz>
2022-04-01 17:07:03 +0300

    finish test ProductControllerTest.php test_store_if_sent_valid_form

--- tests/Feature/Controllers/Admin/ProductControllerTest.php
+++ tests/Feature/Controllers/Admin/ProductControllerTest.php
@@ -3,16 +3,19 @@
 namespace Tests\Feature\Controllers\Admin;
 
 use App\Models\Category;
+use App\Models\Product;
 use App\Models\Role;
 use App\Models\User;
 use Database\Seeders\AdminUserSeeder;
 use Database\Seeders\RolesSeeder;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Http\UploadedFile;
 use Tests\TestCase;
 
 class ProductControllerTest extends TestCase
 {
-    use RefreshDatabase;
+    use RefreshDatabase, WithFaker;
 
     protected User $user;
 
@@ -21,8 +24,8 @@ class ProductControllerTest extends TestCase
         parent::setUp();
         $this->seed(RolesSeeder::class);
         $this->seed(AdminUserSeeder::class);
-        Category::factory(3)->create();
-        User::factory(5)->create();
+        Category::factory(1)->create();
+        User::factory(1)->create();
 
         $this->user = $this->getUser();
     }
@@ -65,15 +68,32 @@ class ProductControllerTest extends TestCase
         $response->assertSee('Create product');
     }
 
-    public function test_store_if_sent_blank_form(): void
+    public function test_store_if_sent_invalid_data(): void
     {
-        $response = $this->actingAs($this->user)->post(route('admin.product.store'));
+        $response = $this->actingAs($this->user)->post(route('admin.product.store', []));
+
         $response->assertStatus(302);
+        $response->assertSessionHasErrors(['title']);
     }
 
     public function test_store_if_sent_valid_form(): void
     {
-        self::assertTrue(true); //TODO implement test
+        $data = [
+            'title' => $this->faker->realTextBetween(5, 10),
+            'description' => $this->faker->realTextBetween(10, 150),
+            'short_description' => $this->faker->realTextBetween(10, 50),
+            'sku' => 55555,
+            'category' => Category::inRandomOrder()->first()->title,
+            'price' => 9999,
+            'discount' => $this->faker->randomFloat(0, 1, 20),
+            'count' => $this->faker->randomFloat(0, 1, 100000),
+            'thumbnail' => UploadedFile::fake()->create('test.png'),
+        ];
+
+        $response = $this->actingAs($this->user)->post(route('admin.product.store'), $data);
+        $this->assertEquals(55555, Product::first()->sku);
+        $this->assertEquals(9999, Product::first()->price);
+        $response->assertStatus(302)->assertRedirect(route('admin.product.index'));
     }
 
     public function test_edit(): void
