31f80ce673be99f5ea9ee1d496d37e0a3c10e669
Serhii <serhiy.voytenko@dn.pebs.biz>
2022-04-05 12:24:23 +0300

    finish CartController.php

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -4,14 +4,17 @@ namespace App\Http\Controllers;
 
 use App\Models\Product;
 use Gloudemans\Shoppingcart\Facades\Cart;
+use Illuminate\Contracts\Support\Renderable;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 
 class CartController extends Controller
 {
-    public function index()
+    public function index(): Renderable
     {
-        return view('cart/index');
+        $cart = Cart::instance('shopping');
+
+        return view('cart/index', compact('cart'));
     }
 
     public function add(Product $product, Request $request): RedirectResponse
@@ -28,13 +31,20 @@ class CartController extends Controller
         return redirect()->back()->with('success', 'The product was success added into the cart.');
     }
 
-    public function remove()
+    public function remove(Request $request): RedirectResponse
     {
+        Cart::instance('shopping')->remove($request->input('rowId'));
 
+        return redirect()->back();
     }
 
-    public function countUpdate()
+    public function countUpdate(Request $request, Product $product): RedirectResponse
     {
+        if ($request->input('product_count') > $product->count){
+            return redirect()->back()->with('warning', 'Available only ' . $product->count . ' ' . $product->title . '.');
+        }
+        Cart::instance('shopping')->update($request->input('rowId'), $request->input('product_count'));
 
+        return redirect()->back();
     }
 }
--- resources/views/cart/index.blade.php
+++ resources/views/cart/index.blade.php
@@ -6,8 +6,13 @@
             <div class="col-md-12">
                 <h3 class="text-center">{{ __('Cart') }}</h3>
             </div>
+            @if(session('warning'))
+                <div class="alert alert-warning">
+                        {{ session('warning') }}
+                </div>
+            @endif
             <div class="col-md-12">
-                @if(Cart::instance('shopping')->count() > 0)
+                @if($cart->count() > 0)
                     <table class="table table-light">
                         <thead>
                         <tr>
@@ -15,42 +20,42 @@
                             <th>Qty</th>
                             <th>Price</th>
                             <th>Subtotal</th>
+                            <th>Delete</th>
                         </tr>
                         </thead>
 
                         <tbody>
 
-                        @each('cart.parts.product_view', Cart::instance('shopping')->content(), 'row')
+                        @each('cart.parts.product_view', $cart->content(), 'row')
 
                         </tbody>
                     </table>
                 @else
                     <h3 class="text-center">There are no products in cart</h3>
                 @endif
-                <hr>
-                <table class="table table-dark" style="width: 50%; float: right;">
+                <table class="table table-primary" style="width: 50%; float: right;">
                     <tbody>
                     <tr>
                         <td colspan="2">&nbsp</td>
                         <td>Subtotal</td>
-                        <td>{{ Cart::subtotal() }}</td>
+                        <td>{{ $cart->subtotal() }}</td>
                     </tr>
                     <tr>
                         <td colspan="2">&nbsp</td>
                         <td>Tax</td>
-                        <td>{{ Cart::tax() }}</td>
+                        <td>{{ $cart->tax() }}</td>
                     </tr>
                     <tr>
                         <td colspan="2">&nbsp</td>
                         <td>Total</td>
-                        <td>{{ Cart::total() }}</td>
+                        <td>{{ $cart->total() }}</td>
                     </tr>
                     </tbody>
                 </table>
             </div>
             @auth
                 <div class="col-md-12 text-right">
-{{--                    <a href="{{ route('checkout') }}" class="btn btn-outline-success">{{ __('Proceed to checkout') }}</a>--}}
+                    <a href="{{ route('home') }}" class="btn btn-outline-success">{{ __('Proceed to checkout') }}</a>
                 </div>
             @endauth
         </div>
